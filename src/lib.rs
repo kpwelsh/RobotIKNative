2a61ca70def87bd75f12ccf365655ee3b6cfaa10
Kevin Welsh <kevinwelsh132@gmail.com>
2022-04-21 13:06:40 -0500

    Refactor to be consistent with the new version of ik_base.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use robot_ik_base;
+use robot_ik_base::IKSolver;
 
 extern crate libc;
 use std::os::raw::c_char;
@@ -8,72 +8,97 @@ use std::error::Error;
 
 
 extern crate k;
-use k::nalgebra as na;
-use na::{Quaternion, UnitQuaternion, Vector3};
-use std::panic;
+use k::{nalgebra as na, Isometry3};
+use na::{Quaternion, UnitQuaternion, Translation3};
 
 
 fn parse_c_str(s: *const c_char) -> Result<&'static str, Box<dyn Error>> {
     Ok(unsafe { CStr::from_ptr(s).to_str()? })
 }
 
+
 #[no_mangle]
-pub extern "cdecl" fn init(urdf: *const c_char, dof: i32) -> i32 {
-    let urdf = parse_c_str(urdf);
-    if urdf.is_err() {
-        return 5;
+pub extern "C" fn new_solver(urdf_ptr: *const c_char, ee_frame_ptr: *const c_char) -> *const IKSolver {
+    let urdf = parse_c_str(urdf_ptr).unwrap();
+    let ee_frame = parse_c_str(ee_frame_ptr).unwrap();
+
+    let mut solver = IKSolver::from_urdf_str(urdf);
+    solver.set_ee(ee_frame);
+
+    
+    Box::into_raw(Box::new(solver))
+}
+
+#[no_mangle]
+pub extern "C" fn set_self_collision(iksolver: *mut IKSolver, self_collision: bool) {
+    unsafe { 
+        match iksolver.as_mut() {
+            Some(solver) => solver.self_collision = self_collision,
+            None => { }
+        }
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn solve(iksolver: *mut IKSolver, current_q_ptr: *mut f32, trans_ptr: *const [f32; 7], q_ptr: *mut f32) -> bool {
+    
+    let iksolver = unsafe { 
+        match iksolver.as_mut() {
+            Some(solver) => solver,
+            None => { return false; }
+        }
+    };
+    let current_q;
+    let trans;
+    unsafe {
+        let dof = iksolver.dof();
+        if dof < 0 {
+            return false;
+        }
+        current_q = Vec::from(std::slice::from_raw_parts(current_q_ptr, dof as usize));
+        trans = std::ptr::read(trans_ptr);
     }
-    let urdf = urdf.unwrap();
-    match robot_ik_base::init(urdf, dof as usize) {
-        Ok(id) => {
-            id as i32
+    let current_q : Vec<f64> = current_q.into_iter().map(|v| v as f64).collect();
+    let trans : Vec<f64> = trans.iter().map(|&v| v as f64).collect();
+    
+
+    let x = Translation3::new(trans[0], trans[1], trans[2]);
+    let rot = UnitQuaternion::from_quaternion(Quaternion::new(trans[3], trans[4], trans[5], trans[6]));
+    let pose = Isometry3::from_parts(x, rot);
+
+    iksolver.solve(&current_q, &pose).ok().and_then(|q| {
+        let q_array;
+        unsafe {
+            q_array = std::slice::from_raw_parts_mut(q_ptr, q.len());
+        };
+        for i in 0..q_array.len() {
+            q_array[i] = q[i] as f32;
+        }
+        Some(())
+    }).is_some()
+}
+
+
+#[no_mangle]
+pub extern "C" fn dof(iksolver: *mut IKSolver) -> i32 {
+    match unsafe { iksolver.as_ref() } {
+        Some(solver) => {
+            solver.dof()
         },
-        Err(_e) => {
+        None => {
             -1
         }
     }
 }
 
+
 #[no_mangle]
-pub extern "cdecl" fn solve(robot_id: i32, current_q_ptr: *mut f32, frame_ptr: *const c_char, trans_ptr: *const [f32;7], q_ptr: *mut f32) -> i32 {
-    let robot_id = robot_id as usize;
-    let frame;
-    frame = parse_c_str(frame_ptr);
-    if frame.is_err() {
-        return -1;
+pub extern "C" fn deallocate(ptr: *mut IKSolver) {
+    if ptr.is_null() {
+        return;
     }
-    let frame = frame.unwrap();
-    let dof = robot_ik_base::dof(robot_id, frame);
-
-    let current_q;
-    let trans;
     unsafe {
-        current_q = Vec::from(std::slice::from_raw_parts(current_q_ptr, dof));
-        trans = std::ptr::read(trans_ptr);
+        println!("{:?}", ptr);
+        Box::from_raw(ptr);
     }
-
-    let x = Vector3::new(trans[0], trans[1], trans[2]);
-    let rot = UnitQuaternion::from_quaternion(Quaternion::new(trans[3], trans[4], trans[5], trans[6]));
-    
-    let result = panic::catch_unwind(|| {
-        match robot_ik_base::solve_32(robot_id, &current_q, &frame, &x, &rot) {
-            Ok(q) => {
-                let q_array;
-                unsafe {
-                    q_array = std::slice::from_raw_parts_mut(q_ptr, dof);
-                };
-                for i in 0..q_array.len() {
-                    q_array[i] = q[i];
-                }
-                0
-            },
-            Err(e) => {
-                e.code
-            }
-        }
-    });
-    if let Ok(b) = result {
-        return b;
-    }
-    return 7;
 }
